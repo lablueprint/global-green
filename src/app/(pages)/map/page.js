f5dca10e9388f2da1bc0366f5c3ebd781eecc6b7
MariusPL <48773928+mariopenglee@users.noreply.github.com>
2023-11-27 22:20:09 -0800

    map made

--- src/app/(pages)/map/page.js
+++ src/app/(pages)/map/page.js
@@ -1,7 +1,25 @@
-import React from 'react';
+'use client';
+import React, { useRef, useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
 import styles from './map.module.css';
 
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFyaW9wZW5nbGVlIiwiYSI6ImNscGh4bWJ5ZDBiNGQycnAzenl3ZGFoMGgifQ.a9h8rGqm200YEYvCS_-szA';
+
 function Map() {
+  const mapContainerRef = useRef(null);
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-74.5, 40], // starting position
+      zoom: 9 // starting zoom
+    });
+
+    // Clean up on unmount
+    return () => map.remove();
+  }, []);
+
   return (
     <>
       <div className={styles.wrapper}>
@@ -11,6 +29,7 @@ function Map() {
         <div className={styles.anotherOne}>
           2nd Example!
         </div>
+        <div ref={mapContainerRef} className={styles.mapContainer} />
       </div>
       <br />
     </>

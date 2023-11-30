1487ad7fb5d3fc146aedf1b9026047cef5ef42c2
vicchois <victoriachoi04@gmail.com>
2023-11-29 20:51:30 -0800

    Map Styles and Marker

--- src/app/(pages)/map/page.js
+++ src/app/(pages)/map/page.js
@@ -1,6 +1,9 @@
 'use client';
+
 import React, { useRef, useEffect } from 'react';
+import Head from 'next/head';
 import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import styles from './map.module.css';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFyaW9wZW5nbGVlIiwiYSI6ImNscGh4bWJ5ZDBiNGQycnAzenl3ZGFoMGgifQ.a9h8rGqm200YEYvCS_-szA';
@@ -13,8 +16,15 @@ function Map() {
       container: mapContainerRef.current,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [-74.5, 40], // starting position
-      zoom: 9 // starting zoom
+      zoom: 9, // starting zoom
     });
+
+    map.addControl(new mapboxgl.NavigationControl());
+
+    const marker = new mapboxgl.Marker({ color: 'black' })
+      .setLngLat([-74.5, 40])
+      .setPopup(new mapboxgl.Popup().setHTML('Location Name'))
+      .addTo(map);
 
     // Clean up on unmount
     return () => map.remove();
@@ -24,10 +34,7 @@ function Map() {
     <>
       <div className={styles.wrapper}>
         <div className={styles.exampleText}>
-          Example text!
-        </div>
-        <div className={styles.anotherOne}>
-          2nd Example!
+          Map!
         </div>
         <div ref={mapContainerRef} className={styles.mapContainer} />
       </div>

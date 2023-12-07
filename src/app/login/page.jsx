6695bf7ac424482707e6b2b35f8d7f32afe9cdbb
Ryan Yeo <ryandc.yeo@gmail.com>
2023-12-06 21:06:19 -0800

    merge + cleanup

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,71 +1,48 @@
-'use client'
-import React, { useState, useEffect, Button } from 'react';
-import styles from './page.module.css';
+'use client';
+
+import React, { useState } from 'react';
+// import styles from './page.module.css';
 
 function Example() {
-
-
-
-  var accounts = {
-    "username": "testing",
-    "password": "testing"
-}
-
-
-
-  
-
-
-  // accounts = {
-  //   "megan":"taruni",
-  //   "taruni":"megan"
-  // };
-
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const accounts = {
+    username: 'testing',
+    password: 'testing',
+  };
 
   const usernameChange = (e) => {
     setUsername(e.target.value);
-    // accounts.username = username; 
+    // accounts.username = username;
   };
 
   const passwordChange = (e) => {
     setPassword(e.target.value);
-    // accounts.password = password; 
+    // accounts.password = password;
+  };
 
-  }
-
-
-  
-
-  
-	
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const submitLog = (event) =>  {
+  const submitLog = (event) => {
     event.preventDefault();
 
     // setPassword(accounts.password);
     // setUsername(accounts.username);
-    if(username == '' || password == ''){
-      alert("Input a username and/or password!!!");
-    }else{
-      //accounts["username"] = username;
-    
+    if (username === '' || password === '') {
+      alert('Input a username and/or password!!!');
+    } else {
+      // accounts["username"] = username;
+
       accounts.username = username;
       accounts.password = password;
       console.log(accounts);
-    
     }
-
-
-  
-  }
+  };
   return (
     <div>
-      <form >
-        <label type ="eAddress">Email Address:</label>
-        
+      <form>
+        <label type="eAddress">Email Address:</label>
+
         <input type="text" id="eAddress" name="eAddress" onChange={usernameChange} />
-        <label name ="pass">Password:</label>
+        <label name="pass">Password:</label>
         <input type="password" id="pass" name="pass" onChange={passwordChange} />
         {/* <input type="email" id="email" name="email" required />
         <input type="submit" value="Submit" /> */}
@@ -74,7 +51,6 @@ function Example() {
 
     </div>
   );
-  
 }
 
 export default Example;

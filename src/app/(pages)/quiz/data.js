a3a483d3e956c3b2b38731087e78ab32c07e9fdd
vicchois <victoriachoi04@gmail.com>
2024-01-08 21:19:46 -0800

    quiz questions

---
+++ src/app/(pages)/quiz/data.js
@@ -0,0 +1,47 @@
+export const quiz = {
+  totalQuestions: 5,
+  questions: [
+    {
+      id: 1,
+      question: 'How many pounds of plastic enter the ocean every day?',
+      options: ['17 Billion Pounds', '33 Billion Pounds', '52 Billion Pounds', '95 Billion Pounds'],
+      answer: '33 Billion Pounds',
+    },
+    {
+      id: 2,
+      question: 'What percentage of marine waste is plastic pollution?',
+      options: ['30%', '50%', '80%', '90%'],
+      answer: '80%',
+    },
+    {
+      id: 3,
+      question: 'What type of plastics make up the majority of the Great Pacific Garbage Patch?',
+      options: ['Microplastics', 'Straws', 'Bags', 'Fishing line'],
+      answer: 'Microplastics',
+    },
+    {
+      id: 4,
+      question: 'By what year is it estimated that there will be more plastic in our oceans than fish?',
+      options: ['2035', '2040', '2045', '2050'],
+      answer: '2050',
+    },
+    {
+      id: 5,
+      question: 'What percentage of marine debris sinks to the ocean floor?',
+      options: ['15%', '40%', '70%', '95%'],
+      answer: '70%',
+    },
+    {
+      id: 6,
+      question: 'How many animals die from getting entangled in plastic debris each year?',
+      options: ['50,000', '100,000', '150,000', '200,000'],
+      answer: '100,000',
+    },
+    {
+      id: 7,
+      question: 'What natural resource(s) are most plastics made from?',
+      options: ['Fossil fuels', 'Cellulose', 'Lignin', 'Glucose'],
+      answer: 'Fossil fuels',
+    },
+  ],
+};

9f3289e3d3e5a9c9529889096be54812d4e3ff6e
mariopenglee <mariopeng@ucla.edu>
2024-01-15 22:05:51 -0800

    hardcode first user
    now we'll fetch the user[0] from the users api, the users api needs to be cleaned up

--- src/app/(pages)/profile/page.jsx
+++ src/app/(pages)/profile/page.jsx
@@ -1,21 +1,54 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import styles from './page.module.css';
 import defaultProfilePic from './profilepic.jpg'; // Assuming you have a default profile pic
 
+
+/*
 const userData = {
-  name: 'Isaac Wen',
+  userName: 'Isaac Wen',
   rank: '1000',
   badges: ['Badge 1', 'Badge 2', 'Badge 3', 'Badge 4', 'Badge 5'],
   courses: ['Course 1', 'Course 2', 'Course 3'],
 };
+*/
 
 function Profile() {
   const [isEditing, setIsEditing] = useState(false);
-  const [editedName, setEditedName] = useState(userData.name);
   const [profileImage, setProfileImage] = useState(defaultProfilePic);
+
+  const [userData, setUserData] = useState({});
+  const [editedName, setEditedName] = useState('');
+
+  useEffect(() => {
+    if (!userData.userName) {
+     async function fetchUserData() {
+        if (localStorage.getItem('userData')) {
+          const data = JSON.parse(localStorage.getItem('userData'));
+          setUserData(data);
+          setEditedName(data.userName);
+          return;
+        }
+        else {
+        const response = await fetch('/api/users');
+        const all_users = await response.json();
+        const data = all_users['users'][0];
+        setUserData(data);
+        setEditedName(data.userName);
+        localStorage.setItem('userData', JSON.stringify(data));
+        console.log('data', data);
+        }
+      }
+      fetchUserData();
+    }
+
+    
+    
+
+  }
+  , [userData]);
 
   const handleNameClick = () => {
     setIsEditing(true);
@@ -72,7 +105,7 @@ function Profile() {
             className={styles.editableInput}
           />
         ) : (
-          <h1 onClick={handleNameClick}>{userData.name}</h1>
+          <h1 onClick={handleNameClick}>{userData.userName}</h1>
         )}
         <p>
           Rank:
@@ -82,23 +115,25 @@ function Profile() {
       <div className={styles.badgesSection}>
         <h2>Badges</h2>
         <div className={styles.row}>
-          {userData.badges.map((badge, index) => (
+          {userData.badges ? userData.badges.map((badge, index) => (
             <div key={index} className={styles.badgeItem}>
               <div className={styles.badgeIcon} />
               <div className={styles.rowName}>{badge}</div>
             </div>
-          ))}
+          ))
+            : null}
         </div>
       </div>
       <div className={styles.coursesSection}>
         <h2>Courses</h2>
         <div className={styles.row}>
-          {userData.courses.map((course, index) => (
+          {userData.courses ? userData.courses.map((course, index) => (
             <div key={index} className={styles.courseItem}>
               <div className={styles.courseIcon} />
               <div className={styles.rowName}>{course}</div>
             </div>
-          ))}
+          ))
+            : null}
         </div>
       </div>
     </div>

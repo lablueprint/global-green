04c073aa28b9680fa304f3802a584d20297638b9
mariopenglee <mariopeng@ucla.edu>
2024-01-22 20:42:54 -0800

    added routing for specific users

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -13,17 +13,6 @@ export async function POST(request) {
   return NextResponse.json({ message: 'User Created' }, { status: 201 });
 }
 
-export async function PUT(request) {
-  const {
-    id, firstName, lastName, email, password, userName, rank, badges, courses,
-  } = await request.json();
-  await connectMongoDB();
-  await User.findByIdAndUpdate(id, {
-    firstName, lastName, email, password, userName, rank, badges, courses,
-  });
-  return NextResponse.json({ message: 'User Updated' }, { status: 200 });
-}
-
 export async function GET() {
   await connectMongoDB();
   const users = await User.find();

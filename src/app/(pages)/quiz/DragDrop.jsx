ec318be2676df9b3b86282a48de8ddbda573c578
vicchois <victoriachoi04@gmail.com>
2024-01-24 21:49:41 -0800

    drag drop question format

--- src/app/(pages)/quiz/DragDrop.jsx
+++ src/app/(pages)/quiz/DragDrop.jsx
@@ -1,13 +1,57 @@
 import Draggable from 'react-draggable';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default function DragDrop() {
+export default function DragDrop({ prompt, options }) {
   return (
     <div>
       <h3>GeeksforGeeks - Draggable Components</h3>
-      <Draggable>
-        <div>We can move this text</div>
-      </Draggable>
+      <p>{prompt}</p>
+      {options.map((option, index) => (
+        <div
+          style={{ display: 'flex' }}
+        >
+          <div
+            style={{
+              margin: '5px',
+              width: '40%',
+              padding: '10px 10px',
+              border: '1px solid black',
+            }}
+          >
+            this is the answer box
+
+          </div>
+          <div
+            key={index}
+            style={{
+              margin: '5px',
+              width: '40%',
+              padding: '10px 10px',
+              border: '1px solid black',
+            }}
+          >
+            {option.question}
+          </div>
+        </div>
+
+      ))}
+      <div style={{ display: 'flex', gap: 5 }}>
+        {options.map((option, index) => (
+          <Draggable>
+            <div
+              key={index}
+              style={{
+                margin: '5px',
+                padding: '10px 10px',
+                border: '1px solid black',
+              }}
+            >
+
+              {option.answer}
+            </div>
+          </Draggable>
+        ))}
+      </div>
     </div>
   );
 }

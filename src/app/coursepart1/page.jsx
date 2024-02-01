ebad3895e681a22eda616e3b4eca2fcb260209fe
Megan Tieu <meganetieu@gmail.com>
2024-01-31 23:12:41 -0800

    img -> image

--- src/app/coursepart1/page.jsx
+++ src/app/coursepart1/page.jsx
@@ -8,7 +8,7 @@ import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
 import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
 import styles from "./page.module.css";
-
+import Image from "next/image";
 function Module({ modules, currentIndex, handlePrevClick, handleNextClick }) {
   const currentModule = modules[currentIndex];
 
@@ -21,10 +21,12 @@ function Module({ modules, currentIndex, handlePrevClick, handleNextClick }) {
       <h1>{currentModule.moduleName}</h1>
       <h2>{currentModule.lessonTitle}</h2>
       <p>{currentModule.text}</p>
-      <img
+      <Image
         className={styles.lessonImage}
         src={currentModule.imageUrl}
         alt="Lesson Image"
+        width={500}
+        height={500}
       />
       <div>
         {currentIndex != 0 && (
@@ -62,21 +64,19 @@ function Example() {
       lessonTitle:
         "Lesson 1: Introduction to the Plastics and Recycling Course",
       text: "Plastic is all around us. It’s in our kitchens, shoes, entertainment systems, vehicles, and even our clothing. It is versatile—it can be light yet durable or soft and malleable—and there is virtually no industry in the world that doesn’t have some application for plastic. Chances are that you can see multiple pieces of one plastic or another in your immediate surroundings. Given its ubiquity, it can be easy to take such an abundance of plastic for granted. But what exactly is plastic, where does it come from, and how has it managed to so radically transform the world we live in since it was first created in the early 20th century? If you find yourself asking questions like these, you’re in the right place! Over the span of this Plastics and Recycling course, we’re going to take a deep dive into a variety of topics relating to the history, manufacture, applications, and hazards of plastic in our world today.",
-      imageUrl: "https://cdn.media.amplience.net/i/hc/img002",
+      imageUrl: "/curiousgeorge.png",
     },
     {
       moduleName: "Course 1: Plastics and Recycling",
       lessonTitle: "Lesson 2: Where Does Plastic Come From?",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
-      imageUrl:
-        "https://cdn.shopify.com/s/files/1/0416/8083/0620/files/blog_tuxedosam-friends_07282022_01-TX-CHIP.jpg?v=1659050063",
+      imageUrl: "/tuxedosam.webp",
     },
     {
       moduleName: "Course 1: Plastics and Recycling",
       lessonTitle: "Lesson 3: Resin Identification Codes",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-      imageUrl:
-        "https://i0.wp.com/babylibrarians.com/wp-content/uploads/2019/02/clifford-the-big-red-dog.jpg?resize=1363%2C480&ssl=1",
+      imageUrl: "/clifford.webp",
     },
   ];
 

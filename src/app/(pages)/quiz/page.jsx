df36628ab68dbc5856e840f611cf95cbd92650fd
Dauniel <danieljchang5@gmail.com>
2024-02-05 23:44:14 -0800

    fixing es lint errors

--- src/app/(pages)/quiz/page.jsx
+++ src/app/(pages)/quiz/page.jsx
@@ -2,11 +2,10 @@
 
 import React, { useState } from 'react';
 import styles from './page.module.css';
-import MultipleChoiceQuiz from './MultipleChoice'; 
+import MultipleChoiceQuiz from './MultipleChoice';
 import Quizzes from './data'; // Assuming this is the path to your Quizzes data
-import LinearWithValueLabel from '../quiz/progressBar';
-import Results from './results'
-
+import LinearWithValueLabel from './progressBar';
+import Results from './results';
 
 function Quiz() {
   const [currentQuizIndex, setCurrentQuizIndex] = useState(0);
@@ -17,23 +16,36 @@ function Quiz() {
   const [points, setPoints] = useState(0);
   const [showResults, setShowResults] = useState(false);
 
-
   const currentQuiz = Quizzes[currentQuizIndex];
   const currentQuestion = currentQuiz.questions[currentQuestionIndex];
 
   const handleAnswer = (isCorrect, selectedOption) => {
-    setSelectedAnswers(prevAnswers => ({
+    setSelectedAnswers((prevAnswers) => ({
       ...prevAnswers,
-      [currentQuestionIndex]: selectedOption
+      [currentQuestionIndex]: selectedOption,
     }
     ));
-    setShowNext(true); 
+    setShowNext(true);
     if (isCorrect) {
-      setProgress(prevProgress => Math.min(prevProgress + (100 / currentQuiz.totalQuestions), 100));
-      setPoints(prevPoints => prevPoints + 1);
+      setProgress((prevProgress) => Math.min(prevProgress
+         + (100 / currentQuiz.totalQuestions), 100));
+      setPoints((prevPoints) => prevPoints + 1);
     }
   };
-  
+
+  const handleNextQuiz = () => {
+    if (currentQuizIndex < Quizzes.length - 1) {
+      setShowResults(true); // Show results instead of next quiz
+    } else {
+      setCurrentQuizIndex(currentQuizIndex + 1);
+      setCurrentQuestionIndex(0);
+      setProgress(0);
+      setShowNext(false);
+      setSelectedAnswers({});
+      setPoints(0);
+    }
+  };
+
   const handleNextQuestion = () => {
     const nextQuestionIndex = currentQuestionIndex + 1;
     if (nextQuestionIndex < currentQuiz.questions.length) {
@@ -52,44 +64,39 @@ function Quiz() {
     }
   };
 
-  const handleNextQuiz = () => {
-    if (currentQuizIndex < Quizzes.length - 1) {
-      setShowResults(true); // Show results instead of next quiz
-    } else {
-      setCurrentQuizIndex(currentQuizIndex + 1);
-      setCurrentQuestionIndex(0);
-      setProgress(0);
-      setShowNext(false);
-      setSelectedAnswers({});
-      setPoints(0);
-    }
-
-  };
-
   if (showResults) {
     return <Results points={points} totalQuestions={currentQuiz.totalQuestions} />;
   }
 
-  const handlePreviousQuiz = () => {
-    if (currentQuizIndex > 0) {
-      setCurrentQuizIndex(currentQuizIndex - 1);
-      setCurrentQuestionIndex(0);
-      setProgress(0);
-      setShowNext(false);
-      setSelectedAnswers({});
-      setPoints(0);
-    }
-  };
+  // const handlePreviousQuiz = () => {
+  //   if (currentQuizIndex > 0) {
+  //     setCurrentQuizIndex(currentQuizIndex - 1);
+  //     setCurrentQuestionIndex(0);
+  //     setProgress(0);
+  //     setShowNext(false);
+  //     setSelectedAnswers({});
+  //     setPoints(0);
+  //   }
+  // };
 
   return (
     <div className={styles.container}>
       <div className={styles.quizContainer}>
         <LinearWithValueLabel value={progress} />
         <div className={styles.quizQuestionNumber}>
-          Question {currentQuestionIndex + 1} of {currentQuiz.totalQuestions}
+          Question
+          {' '}
+          {currentQuestionIndex + 1}
+          {' '}
+          of
+          {' '}
+          {currentQuiz.totalQuestions}
         </div>
         <div>
-          <strong>Points: {points}</strong>
+          <strong>
+            Points:
+            {points}
+          </strong>
         </div>
         <MultipleChoiceQuiz
           key={currentQuestion.id}
@@ -99,14 +106,16 @@ function Quiz() {
           handleAnswer={handleAnswer}
           selectedAnswer={selectedAnswers[currentQuestionIndex]}
         />
-        <div className={currentQuestionIndex > 0 ? styles.buttonsContainerWithBack : styles.buttonsContainer}>
+        <div className={currentQuestionIndex > 0
+          ? styles.buttonsContainerWithBack : styles.buttonsContainer}
+        >
           {currentQuestionIndex > 0 && (
-            <button className={styles.backButton} onClick={handlePreviousQuestion}>
+            <button type="button" className={styles.backButton} onClick={handlePreviousQuestion}>
               Back
             </button>
           )}
           {showNext && (
-            <button className={styles.nextButton} onClick={handleNextQuestion}>
+            <button type="button" className={styles.nextButton} onClick={handleNextQuestion}>
               Next
             </button>
           )}

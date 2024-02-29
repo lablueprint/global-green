4fbaed5f4c5d6b90d8000e91f79e78a2ca8e8998
James He <he.is.james7@gmail.com>
2024-02-28 22:14:49 -0800

    started styling

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -2,6 +2,7 @@
 import React, {useState, useEffect } from 'react';
 import Link from 'next/link';
 import styles from "./navbar.module.css"
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
 
 export default function NavBar() {
     const [currentPath, setCurrentPath] = useState('');
@@ -23,12 +24,17 @@ export default function NavBar() {
 return (
     <div className={styles.navbar}>
         <div className={styles.navcomp}>
+            <div className={styles.personIcon}>
+                <PersonOutlinedIcon style={{justifySelf: 'center', position: 'fixed'}} />
+            </div>
             <NavLink href="/" style={{ textDecoration: 'none' }} className={`${styles.link} ${styles.GGScholar}`}>GG Scholar</NavLink>
             <NavLink href="/courses" style={{ textDecoration: 'none' }} className={styles.link}>Courses</NavLink>
             <NavLink href="/challenges" style={{ textDecoration: 'none' }} className={styles.link}>Challenges</NavLink>
-            <NavLink href="/map" style={{ textDecoration: 'none' }} className={styles.link}>Map</NavLink>
             <NavLink href="/store" style={{ textDecoration: 'none' }} className={styles.link}>Store</NavLink>
-            <NavLink href="/profile" style={{ textDecoration: 'none' }} className={styles.link}>Profile</NavLink>
+            <div className={styles.navBottom}>
+                <NavLink href="/map" style={{ textDecoration: 'none' }} className={styles.link}>Map</NavLink>
+                <NavLink href="/profile" style={{ textDecoration: 'none' }} className={styles.link}>Profile</NavLink>
+            </div>
         </div>
     </div>
 );

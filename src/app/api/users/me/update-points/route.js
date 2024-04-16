c718f1a3922a25f98b7b2003f65f212c1e04d769
Megan Tieu <meganetieu@gmail.com>
2024-04-15 22:11:27 -0700

    update points patch request

---
+++ src/app/api/users/me/update-points/route.js
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server';
+import connectMongoDB from '../../../../../../libs/mongodb';
+import User from '../../../../../../models/user';
+
+export async function PATCH(request) {
+  await connectMongoDB();
+  try {
+    const reqBody = await request.json();
+    const { userId, points } = reqBody;
+
+
+    if (!points) {
+      return NextResponse.json({ error: 'Points must be a number and is required' }, { status: 400 });
+    }
+
+    const user = await User.findByIdAndUpdate(userId, { points }, { new: true }).select('-password');
+
+    if (!user) {
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    }
+
+    return NextResponse.json({ message: 'Points updated successfully', user });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}

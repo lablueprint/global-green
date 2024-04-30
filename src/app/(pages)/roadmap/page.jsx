6ddb9d8fe4624f086072dc44b4059f88b11a4a15
megaminding <meganetieu@gmail.com>
2024-04-29 22:32:58 -0700

    minor change

--- src/app/(pages)/roadmap/page.jsx
+++ src/app/(pages)/roadmap/page.jsx
@@ -1,9 +1,9 @@
 // src/app/(pages)/roadmap/page.jsx
 
-'use client';
+"use client";
 
-import React from 'react';
-import { useRouter } from 'next/navigation';
+import React from "react";
+import { useRouter } from "next/navigation";
 
 function RoadmapsIndexPage() {
   const router = useRouter();
@@ -11,13 +11,69 @@ function RoadmapsIndexPage() {
     <div>
       <h1>Roadmaps</h1>
       <ul>
-        <button type="button" onClick={() => router.push('/roadmap/course/?courseKey=introduction')}> Introduction</button>
-        <button type="button" onClick={() => router.push('/roadmap/course/?courseKey=plastic-and-recycling')}> Plastic and Recycling</button>
-        <button type="button" onClick={() => router.push('/roadmap/course/?courseKey=sustainability-labs')}> Sustainability Labs</button>
-        <button type="button" onClick={() => router.push('/roadmap/course/?courseKey=conservation-and-restoration')}> Conservation and Restoration</button>
-        <button type="button" onClick={() => router.push('/roadmap/course/?courseKey=climate-change')}> Climate Change</button>
-        <button type="button" onClick={() => router.push('/roadmap/course/?courseKey=eco-friendly-travel')}> Eco-Friendly Travel</button>
-        <button type="button" onClick={() => router.push('/roadmap/course/?courseKey=ocean-pollution')}> Ocean Pollution</button>
+        <button
+          type="button"
+          onClick={() => router.push("/roadmap/course/?courseKey=introduction")}
+        >
+          {" "}
+          Introduction
+        </button>
+        <button
+          type="button"
+          onClick={() =>
+            router.push("/roadmap/course/?courseKey=plastic-and-recycling")
+          }
+        >
+          {" "}
+          Plastic and Recycling
+        </button>
+        <button
+          type="button"
+          onClick={() =>
+            router.push("/roadmap/course/?courseKey=sustainability-labs")
+          }
+        >
+          {" "}
+          Sustainability Labs
+        </button>
+        <button
+          type="button"
+          onClick={() =>
+            router.push(
+              "/roadmap/course/?courseKey=conservation-and-restoration"
+            )
+          }
+        >
+          {" "}
+          Conservation and Restoration
+        </button>
+        <button
+          type="button"
+          onClick={() =>
+            router.push("/roadmap/course/?courseKey=climate-change")
+          }
+        >
+          {" "}
+          Climate Change
+        </button>
+        <button
+          type="button"
+          onClick={() =>
+            router.push("/roadmap/course/?courseKey=eco-friendly-travel")
+          }
+        >
+          {" "}
+          Eco-Friendly Travel
+        </button>
+        <button
+          type="button"
+          onClick={() =>
+            router.push("/roadmap/course/?courseKey=ocean-pollution")
+          }
+        >
+          {" "}
+          Ocean Pollution
+        </button>
       </ul>
     </div>
   );

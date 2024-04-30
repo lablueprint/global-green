193fc6b9fa7a62d529bc945dfbc4d80e18f2c738
mariopenglee <mariopeng@ucla.edu>
2024-04-29 22:33:47 -0700

    Update page.jsx

--- src/app/(pages)/roadmap/course/page.jsx
+++ src/app/(pages)/roadmap/course/page.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import Roadmap from '../roadmap';
 
 function CourseRoadmap({ params, searchParams }) {
-  const { courseKey, currStage } = searchParams;
+  const { courseKey } = searchParams;
   return (
     <Roadmap
       courseKey={courseKey}
       title={courseKey}
-      currStage={currStage}
+      currStage={6}
       courseInfo={`This is the course information for ${courseKey}`}
     />
   );

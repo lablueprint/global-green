a58233a9f49cf3566eee02eaf76703434e778d96
megaminding <meganetieu@gmail.com>
2024-05-06 20:28:31 -0700

    loading with Earth spinning. also cleaned up AWS

--- src/app/(pages)/roadmap/roadmap.jsx
+++ src/app/(pages)/roadmap/roadmap.jsx
@@ -1,12 +1,15 @@
-'use client';
+"use client";
 
-import React from 'react';
-import LinearWithValueLabel from './progressbar';
-import styles from './page.module.css';
-import RoadmapSVG from './roadmapSVG';
+import React from "react";
+import LinearWithValueLabel from "./progressbar";
+import styles from "./page.module.css";
+import RoadmapSVG from "./roadmapSVG";
 
 function Roadmap({
-  courseKey, title, currStage, courseInfo = "Plastic is all around us. It's in our kitchens, shoes, entertainment systems, vehicles, and even our clothing. Lorem ipsum...",
+  courseKey,
+  title,
+  currStage,
+  courseInfo = "Plastic is all around us. It's in our kitchens, shoes, entertainment systems, vehicles, and even our clothing. Lorem ipsum...",
 }) {
   const totalProgress = (currStage / 6) * 100;
 
@@ -26,19 +29,13 @@ function Roadmap({
           value={totalProgress}
           x={currStage}
           y={6}
-          style={{ width: '50%' }}
+          style={{ width: "50%" }}
         />
-        <div className={styles.courseInformation}>
-          {courseInfo}
-        </div>
+        <div className={styles.courseInformation}>{courseInfo}</div>
       </div>
       <div className={styles.roadmapContainer}>
-        {RoadmapSVG
-        && (
-        <RoadmapSVG
-          courseKey={courseKey}
-          currStage={currStage}
-        />
+        {RoadmapSVG && (
+          <RoadmapSVG courseKey={courseKey} currStage={currStage} />
         )}
       </div>
     </div>

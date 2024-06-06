eeae58e808e7e27d4887dd1882b62e382a7450ce
Juliet Zhang <julietzhang3@gmail.com>
2024-06-05 21:27:20 -0700

    Add Onboarding videos and descriptions; fix styling

--- src/app/(pages)/onboarding/page.js
+++ src/app/(pages)/onboarding/page.js
@@ -6,18 +6,26 @@ import styles from './page.module.css';
 
 function Onboarding() {
   const router = useRouter();
-  const images = [
-    { content: 'Slide 1' },
-    { content: 'Slide 2' },
-    { content: 'Slide 3' },
-    { content: 'Slide 4' },
-    { content: 'Slide 5' },
+  const videos = [
+    '/RoadmapNavOnboarding.mov',
+    '/GardenStoreOnboarding.mov',
+    '/ChallengesOnboarding.mov',
+  ];
+  const topics = [
+    'Roadmap and Navigation',
+    'Garden and Store',
+    'Challenges',
+  ];
+  const descriptions = [
+    'Pick any course to start learning! Use the roadmap feature to get to the lesson content by clicking on the tile. Click the button in the top left to get back to the roadmap from the lesson page.',
+    'When you finish a course, you earn points. You can use your points to buy accessories and backgrounds in the Store for your Garden. Let\'s walk through accessing your Garden from the Home page.',
+    'Completing certain tasks will earn you points that you can spend in the Store.',
   ];
 
   const [currentSlide, setCurrentSlide] = useState(0);
 
   function nextSlide() {
-    if (currentSlide === images.length - 1) {
+    if (currentSlide === videos.length - 1) {
       router.push('/landing');
     } else {
       setCurrentSlide(currentSlide + 1);
@@ -34,18 +42,30 @@ function Onboarding() {
         <div className={styles.title}>
           Global Green Scholar
         </div>
+        <div className={styles.topic}>
+          {topics[currentSlide]}
+        </div>
+        <div className={styles.description}>
+          {descriptions[currentSlide]}
+        </div>
         <div className={styles['carousel-container']}>
-          <div className={styles['carousel-slide']}>
-            <div className={styles['carousel-content']}>{images[currentSlide].content}</div>
-          </div>
+          <video
+            className={styles['carousel-slide']}
+            autoPlay
+            muted
+            loop
+            controls
+            src={videos[currentSlide]}
+          />
         </div>
         <div className={styles.buttons}>
           <button type="button" className={styles['carousel-button']} onClick={prevSlide}>
             Previous
           </button>
           <div className={styles.dots}>
-            {images.map((image, index) => (
+            {videos.map((video, index) => (
               <div
+                key={index} // Added key prop with unique index
                 className={currentSlide !== index ? styles.dot : styles['active-dot']}
                 onClick={() => { setCurrentSlide(index); }}
               />

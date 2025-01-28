5d750ff32f50c7ccd3cb9fa775360ec63f09e2b9
Ryan Yeo <ryandc.yeo@gmail.com>
2025-01-27 20:28:27 -0800

    eslint production disable

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
 
 module.exports = nextConfig;
 

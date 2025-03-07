7eeed8bcc9255d56c7537c504b6225db29b6e7e7
Ryan Yeo <ryandc.yeo@gmail.com>
2025-03-06 17:58:31 -0800

    nextauth works, adding env to next.config

--- next.config.js
+++ next.config.js
@@ -20,6 +20,9 @@ const nextConfig = {
   env: {
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    GOOGLE_ID: process.env.GOOGLE_ID,
+    GOOGLE_SECRET: process.env.GOOGLE_SECRET,
+    RECAPTCHA_SECRET_KEY: process.env.RECAPTCHA_SECRET_KEY,
   },
 };
 
